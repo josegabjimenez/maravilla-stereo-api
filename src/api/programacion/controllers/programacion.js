38dab9961ccc11cb3b7e973a2d108e43721d23ae
Jose Gabriel Jiménez Vidales <josegabojimenez@gmail.com>
2022-04-27 19:56:45 -0500

    :art: API Setup (entities were defined)

---
+++ src/api/programacion/controllers/programacion.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  programacion controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::programacion.programacion');
