02b492408f621226fdecb06476b8787a761f86af
Jose Gabriel Jiménez <josegabojimenez@gmail.com>
2022-05-01 12:19:32 -0500

    :art: PAUTA Collection was added

---
+++ src/api/pauta/controllers/pauta.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  pauta controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::pauta.pauta');
